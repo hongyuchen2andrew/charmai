e41083f31364d00ae9b6019a0962f1759a169cc6
hongyuchen2andrew <151486368+hongyuchen2andrew@users.noreply.github.com>
2024-03-06 15:56:34 -0800

    Update Account.py

--- pages/Account.py
+++ pages/Account.py
@@ -4,11 +4,17 @@ from streamlit_extras.switch_page_button import switch_page
 
 st.markdown("<h2 style='color:pink; font-size: 60px; text-align:left; margin-top: -40px'>Account</h2>", unsafe_allow_html=True) 
 
-key = st.text_input(label = 'OpenAI API Key (Required)', placeholder = 'Please enter your OpenAI API Keys...')
-st.session_state.api_key = key
-if not key:
-    st.error("Please input your OpenAI API Key.\
-              Don't have a key? Click here: https://openai.com/blog/openai-api")
+api_key = st.text_input(label = 'OpenAI API Key (Required)', placeholder = 'Please enter your OpenAI API Keys...')
+perplexity_key = st.text_input(label = 'Perplexity API Key', placeholder = 'Please enter your Perplexity API key...')
+google_key = st.text_input(label = 'Google API Key', placeholder = 'Please enter your Google API key...')
+st.session_state.api_key = api_key
+st.session_state.perplexity_key = perplexity_key
+st.session_state.google_key = google_key
+if (not api_key) or (not perplexity_key) or (not google_key):
+    st.error("Please input your API Key in the sidebar.\
+              Don't have a OpenAI key? Click here: https://openai.com/blog/openai-api\
+              Don't have a Perplexity key? Click here: https://www.perplexity.ai/settings/api\
+              Don't have a Google key? Click here: https://serpapi.com/.")
     st.stop() 
 st.write('**API Key verified! Let\'s start to complete your profile! You can also complete it later.**')
 userName = st.text_input(label = '**Name** (Optinal)', placeholder = 'Please enter your name...')
@@ -17,14 +23,14 @@ gender = st.text_input(label = '**Gender** (Optinal)', placeholder = 'Please ent
 job = st.text_input(label = '**Job** (Optinal)', placeholder = 'Please enter your job...')
 personality = st.text_input(label = '**Personality** (Personality can be a description or several keywords) (Optinal)', placeholder = 'Please enter your personalities...')
 hobby = st.text_input(label = '**Hobby** (Hobby can be a description or several keywords) (Optinal)', placeholder = 'Please enter your hobbies...')
-#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
+
 col1, col2 = st.columns(2)
 with col2:
     if st.button("Not Now"):
         switch_page("Chatbot")
 if userName or gender or job or personality or hobby:
     prompt = f'Hi, my name is {userName}, my age is {age}, my gender is {gender}, my job is {job}, my personalities are {personality}, my hobbies are {hobby}.'
-    profile = Profile(prompt, key)
+    profile = Profile(prompt, api_key)
     userAge, userGender, userCareer, userPersonality, userHobby = profile.returnProfile()
     st.session_state.userName = userName
     st.session_state.userAge = userAge
