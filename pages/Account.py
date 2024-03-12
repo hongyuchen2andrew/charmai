2906f7361d6b3f09ccb6744911c6ad95a3c64abd
hongyuchen2andrew <151486368+hongyuchen2andrew@users.noreply.github.com>
2024-03-11 18:31:32 -0700

    Update Account.py

--- pages/Account.py
+++ pages/Account.py
@@ -4,27 +4,23 @@ from streamlit_extras.switch_page_button import switch_page
 
 st.markdown("<h2 style='color:pink; font-size: 60px; text-align:left; margin-top: -40px'>Account</h2>", unsafe_allow_html=True) 
 
-api_key = st.text_input(label = 'OpenAI API Key (Required)', placeholder = 'Please enter your OpenAI API Keys...')
-perplexity_key = st.text_input(label = 'Perplexity API Key', placeholder = 'Please enter your Perplexity API key...')
-google_key = st.text_input(label = 'Google API Key', placeholder = 'Please enter your Google API key...')
-# st.session_state.api_key = api_key
-# st.session_state.perplexity_key = perplexity_key
-# st.session_state.google_key = google_key
-
 st.session_state.api_key = st.secrets["api_key"]
 st.session_state.perplexity_key = st.secrets["perplexity_key"]
 st.session_state.google_key = st.secrets["google_key"]
 st.session_state.llama_key = st.secrets["llama_key"]
 st.session_state.langChain_key = st.secrets["langChain_key"]
-if not(("api_key" in st.session_state) and ("perplexity_key" in st.session_state) and ("google_key" in st.session_state) and ("llama_key" in st.session_state) and ("langChain_key" in st.session_state)):
-    st.error("Please input your API Key in the sidebar.\
-              Don't have a OpenAI key? Click here: https://openai.com/blog/openai-api\
-              Don't have a Perplexity key? Click here: https://www.perplexity.ai/settings/api\
-              Don't have a Google key? Click here: https://serpapi.com/.)\
-              Don't have a Llama key? Click here: https://replicate.com/account/api-tokens.)\
-              Don't have a LangChain key? Click here: https://python.langchain.com/docs/get_started/quickstart.")
-    st.stop() 
-st.write('**API Key verified! Let\'s start to complete your profile! You can also complete it later.**')
+# api_key = st.text_input(label = 'OpenAI API Key (Required)', placeholder = 'Please enter your OpenAI API Keys...')
+# perplexity_key = st.text_input(label = 'Perplexity API Key', placeholder = 'Please enter your Perplexity API key...')
+# google_key = st.text_input(label = 'Google API Key', placeholder = 'Please enter your Google API key...')
+# if not(("api_key" in st.session_state) and ("perplexity_key" in st.session_state) and ("google_key" in st.session_state) and ("llama_key" in st.session_state) and ("langChain_key" in st.session_state)):
+#     st.error("Please input your API Key in the box.\
+#               Don't have a OpenAI key? Click here: https://openai.com/blog/openai-api\
+#               Don't have a Perplexity key? Click here: https://www.perplexity.ai/settings/api\
+#               Don't have a Google key? Click here: https://serpapi.com/.)\
+#               Don't have a Llama key? Click here: https://replicate.com/account/api-tokens.)\
+#               Don't have a LangChain key? Click here: https://python.langchain.com/docs/get_started/quickstart.")
+#     st.stop() 
+# st.write('**API Key verified! Let\'s start to complete your profile! You can also complete it later.**')
 userName = st.text_input(label = '**Name** (Optinal)', placeholder = 'Please enter your name...')
 age = st.select_slider('**Age** (Optinal)', options=list(range(18, 121)))
 gender = st.text_input(label = '**Gender** (Optinal)', placeholder = 'Please enter your gender...')
